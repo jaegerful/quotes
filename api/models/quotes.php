8e9667c0bcf7da0c3e3e50564bbb8fde50e3025f
Jaegerly <teamcocohead@gmail.com>
2023-03-09 10:56:36 -0500

    api for quotes

---
+++ api/models/quotes.php
@@ -0,0 +1,232 @@
+<?php
+    /* helper functions to handle json. */
+
+    include_once("helpers.php");
+    include_once("../helpers.php");
+
+    /* this model uses other models. */
+
+    include_once("categories.php");
+    include_once("authors.php");
+
+    class Quotes {
+
+        private $client;
+
+        public function __construct($client) {
+            $this->client = $client;
+        }
+
+        /* each method corresponds to a particular request. */
+
+        /* get all quotes. */
+
+        public function get_quotes() {
+            $query = 
+                "SELECT *
+                 FROM quotes;"
+            ;
+
+            try {
+                $statement = $this->client->connection->prepare($query);
+                $statement->execute();
+                $result = $statement->fetchAll();
+    
+                if (empty($result))
+                    return encode(["message" => "no quotes found"]);
+    
+                return encode($result);
+            }
+
+            catch(PDOException $error) {
+                return encode($error);
+            }
+        }
+
+        /* get quotes by a specific author. */
+
+        public function get_quotes_by_author($id) {
+            $query = 
+                "SELECT *
+                 FROM quotes
+                 WHERE author_id = :id;"
+            ;
+
+            try {
+                $statement = $this->client->connection->prepare($query);
+                $statement->execute(['id' => $id]);
+                $result = $statement->fetchAll();
+    
+                if (empty($result))
+                    return encode(["message" => "no quotes found"]);
+    
+                return encode($result);
+            }
+
+            catch(PDOException $error) {
+                return encode($error);
+            }
+        }
+
+        /* get quotes by a specific category. */
+
+        public function get_quotes_by_category($id) {
+            $query = 
+                "SELECT *
+                 FROM quotes
+                 WHERE category_id = :id;"
+            ;
+
+            try {
+                $statement = $this->client->connection->prepare($query);
+                $statement->execute(['id' => $id]);
+                $result = $statement->fetchAll();
+    
+                if (empty($result))
+                    return encode(["message" => "no quotes found"]);
+    
+                return encode($result);
+            }
+
+            catch(PDOException $error) {
+                return encode($error);
+            }
+        }
+
+        /* get quotes by a specific category and author. */
+
+        public function get_quotes_by_category_and_author($category, $author) {
+            $query = 
+                "SELECT *
+                 FROM quotes
+                 WHERE 
+                    category_id = :category AND 
+                    author_id = :author;"
+            ;
+
+            try {
+                $statement = $this->client->connection->prepare($query);
+                $statement->execute(['category' => $category, "author" => $author]);
+                $result = $statement->fetchAll();
+    
+                if (empty($result))
+                    return encode(["message" => "no quotes found"]);
+    
+                return encode($result);
+            }
+
+            catch(PDOException $error) {
+                return encode($error);
+            }
+        }
+
+
+        /* post a quote. */
+
+        public function post_quote($quote, $category_id, $author_id) {
+            $query = 
+                "INSERT INTO quotes(quote, category_id, author_id)
+                 VALUES (:quote, :category, :author)
+                 RETURNING *;"
+            ;
+
+            try {
+                $statement = $this->client->connection->prepare($query);
+                $statement->execute(['quote' => $quote, 'category' => $category_id, 'author' => $author_id]);
+                $result = $statement->fetch();
+    
+                if (empty($result))
+                    return encode(["message" => "category could not be created"]);
+    
+                return "created quote (" . $result["id"] . ", " . $result["quote"] . ", " . $result["author_id"] . ", " . $result["category_id"] . ")";
+            }
+            
+            catch(PDOException) {
+                
+                /* if exception thrown, the category and/or author identifiers are wrong.  */
+                /* to determine which identifiers are wrong, test them on their respective models. */
+
+                $categories = new Categories($this->client);
+                $authors = new Authors($this->client);
+
+                $tests = [
+                    "category_id" => decode($categories->get_category($category_id)),
+                    "author_id" => decode($authors->get_author($author_id))
+                ];
+
+                if (is_array($tests["category_id"]) && array_key_exists("message", $tests["category_id"]))
+                    $tests["category_id"] = false;
+                
+                if (is_array($tests["author_id"]) && array_key_exists("message", $tests["author_id"]))
+                    $tests["author_id"] = false;
+
+                /* if both identifiers were wrong. */
+
+                if (!$tests["category_id"] && !$tests["author_id"])
+                    return encode(["message" => "category_id and author_id not found"]);
+                
+                /* if only 'category_id' wrong. */
+
+                if (!$tests["category_id"] && $tests["author_id"])
+                    return encode(["message" => "category_id not found"]);
+                
+                /* if only 'author_id' wrong. */
+
+                if (!$tests["author_id"] && $tests["category_id"])
+                    return encode(["message" => "author_id not found"]);
+            }
+        }
+
+        /* update a quote. */
+
+        public function update_quote($id, $quote, $category, $author) {
+            $query = 
+                "UPDATE quotes
+                 SET quote = :quote, category_id = :category, author_id = :author
+                 WHERE id = :id
+                 RETURNING *;"
+            ;
+
+            try {
+                $statement = $this->client->connection->prepare($query);
+                $statement->execute(['id' => $id, 'quote' => $quote, 'category' => $category, 'author' => $author]);
+                $result = $statement->fetch();
+
+                if (empty($result))
+                    return encode(["message" => "no quotes found"]);
+
+                return "updated quote (" . $result["id"] . ", " . $result["quote"] . ", " . $result["author_id"] . ", " . $result["category_id"] . ")";
+            }
+            
+            catch(PDOException $error) {
+                return encode($error);
+            }
+        }
+
+        /* delete a quote. */
+
+        public function delete_quote($id) {
+            $query = 
+                "DELETE FROM quotes
+                 WHERE id = :id
+                 RETURNING id;"
+            ;
+
+            try {
+                $statement = $this->client->connection->prepare($query);
+                $statement->execute(['id' => $id]);
+                $result = $statement->fetch();
+
+                if (empty($result))
+                    return encode(["message" => "no quotes found"]);
+
+                return encode($result);
+            }
+            
+            catch(PDOException $error) {
+                return encode($error);
+            }
+        }
+
+    }
+?>
---
+++ api/quotes/index.php
@@ -0,0 +1,137 @@
+<?php
+    /* response headers. */
+
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
+
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    /* 'options' request. */
+
+    if ($method === "OPTIONS") {
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+        header("Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With");
+        
+        exit();
+    }
+
+    include_once("../database/database.php");
+    $client = new DatabaseClient();
+
+    include_once("../models/quotes.php");
+    $quotes = new Quotes($client);
+
+    /* 'get' request. */
+
+    if ($method === "GET") {
+
+        /* if 'categoryId' and 'authorId' query parameters provided. */
+
+        if (array_key_exists("categoryId", $_GET) && array_key_exists("authorId", $_GET)) {
+            $category = $_GET["categoryId"];
+            $author = $_GET["authorId"];
+
+            $result = $quotes->get_quotes_by_category_and_author($category, $author);
+
+            echo $result;
+            exit();    
+        }
+        
+        /* if 'categoryId' query parameter was provided. */
+        
+        if (array_key_exists("categoryId", $_GET)) {
+            $id = $_GET["categoryId"];
+            $result = $quotes->get_quotes_by_category($id);
+            echo $result;
+            exit();    
+        }
+
+        /* if 'authorId' query parameter was provided. */
+
+        if (array_key_exists("authorId", $_GET)) {
+            $id = $_GET["authorId"];
+            $result = $quotes->get_quotes_by_author($id);
+            echo $result;
+            exit();    
+        }
+        
+        /* otherwise, fetch all quotes. */
+
+        $result = $quotes->get_quotes();
+        echo $result;
+        exit();
+    }
+
+    /* remaining requests have body. */
+            
+    include_once("../helpers.php");
+    $body = decode(file_get_contents("php://input"));
+    
+    /* 'post' request. */
+
+    if ($method === "POST") {
+
+        /* if request does not provide quote, category, or author. */
+
+        if (!array_key_exists("quote", $body) || !array_key_exists("categoryId", $body) || !array_key_exists("authorId", $body)) {
+            $result = encode(["message" => "missing required parameters"]);
+            echo $result;
+            exit();
+        }
+
+        /* otherwise, create quote. */
+
+        $quote = $body["quote"];
+        $category = $body["categoryId"];
+        $author = $body["authorId"];
+
+        $result = $quotes->post_quote($quote, $category, $author);
+        echo $result;
+        exit();
+    }
+
+    /* 'put' request. */
+
+    if ($method === "PUT") {
+
+        /* if request does not provide quote, category, and author. */
+        
+        if (!array_key_exists("quote", $body) || !array_key_exists("categoryId", $body) || !array_key_exists("authorId", $body)) {
+            $result = encode(["message" => "missing required parameters"]);
+            echo $result;
+            exit();
+        }
+  
+        /* otherwise, update author. */
+
+        $id = $body["id"];
+        $quote = $body["quote"];
+        $category = $body["categoryId"];
+        $author = $body["authorId"];
+
+        $result = $quotes->update_quote($id, $quote, $category, $author);
+        echo $result;
+
+        exit();
+    }
+
+    /* 'delete' request. */
+
+    if ($method === "DELETE") {
+
+        /* if request does not provide identifier for author. */
+
+        if (!array_key_exists("id", $body)) {
+            $result = encode(["message" => "missing required parameters"]);
+            echo $result;
+            exit();
+        }
+        
+        /* otherwise, delete author. */
+
+        $id = $body["id"];
+        $result = $quotes->delete_quote($id);
+        echo $result;
+        exit();
+    }
+?>
